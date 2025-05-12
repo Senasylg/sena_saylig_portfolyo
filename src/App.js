e29bddb0e583379239ed3667f18e3eac603d03ba
senasylg <senasaylig@gmail.com>
2025-05-12 17:06:50 +0300

    merhaba commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Merhaba</p>
+        <p>Merhaba Dünya</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
