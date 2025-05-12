c4e670c3e35c4b95eced2b709c869f1f622c6f2d
senasylg <senasaylig@gmail.com>
2025-05-12 17:09:24 +0300

    new1 commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Merhaba Dünya</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
