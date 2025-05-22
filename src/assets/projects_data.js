0120a2c9603c3a8eb69ace55a0f2a5fc20ca0e66
senasylg <senasaylig@gmail.com>
2025-05-22 17:13:25 +0300

    new commit

--- src/assets/projects_data.js
+++ src/assets/projects_data.js
@@ -24,28 +24,28 @@ const projects_data = [
         p_no: 3,
         p_name: "Elma Ambalaj Hattı Tasarımı",
         p_img: project3_img,
-        description: "SolidWorks ile tasarlanan elma ambalaj hattı, dizim, karton yerleştirme, kapak kapama ve etiketleme gibi temel işlemleri içeren verimli bir sistemdir. Amaç, süreci hızlandırarak iş gücü verimliliğini artırmaktır.",
+        description: "SolidWorks ile tasarladığım elma ambalaj hattı, dizim, karton yerleştirme, kapak kapama ve etiketleme gibi temel işlemleri içeren verimli bir sistemdir. Amaç, süreci hızlandırarak iş gücü verimliliğini artırmaktır.",
         tools: ["SOLIDWORKS"]
     },
     {
         p_no: 4,
         p_name: "Kavşak Sinyalize Trafik Işığı Simülasyonu",
         p_img: project4_img,
-        description: "Balıkesir Yeniçayırhisar kavşağı için yapılan projede, trafik akışını iyileştirmek amacıyla sinyalize trafik ışığı sistemi modellemesi ve simülasyonu yapılmıştır. Gerçek verilerle araç yoğunluğu ve bekleme süreleri analiz edilerek, farklı ışıklandırma senaryolarının etkileri değerlendirilmiştir.",
+        description: "Balıkesir Yeniçayırhisar kavşağı için çalıştığım projede, trafik akışını iyileştirmek amacıyla sinyalize trafik ışığı sistemi modellemesi ve simülasyonu yaptım. Gerçek verilerle araç yoğunluğu ve bekleme süreleri analiz edilerek, farklı ışıklandırma senaryolarının etkilerini değerlendirdim.",
         tools: ["Arena Rockwell Simulation", "Minitab", "Excel"]
     },
     {
         p_no: 5,
         p_name: "Stok Takip Sistemi",
         p_img: project5_img,
-        description: "TechDepot teknoloji mağazasının stok yönetimini optimize etmek için geliştirilen bu sistem, iki versiyon halinde uygulanmıştır. İlk versiyon C# Windows Forms ve MS SQL Server, ikinci versiyon ise PyQt5, Qt Designer ve SQLite kullanılarak geliştirilmiştir. Sistem; ürün yönetimi, stok takip, satış, müşteri ve personel yönetimi, raporlama ve analiz modüllerini içermektedir.",
+        description: "TechDepot teknoloji mağazasının stok yönetimini optimize etmek için geliştirdiğim bu sistem, iki versiyon halinde uygulanmıştır. İlk versiyon C# Windows Forms ve MS SQL Server, ikinci versiyon ise PyQt5, Qt Designer ve SQLite kullanılarak geliştirilmiştir. Sistem; ürün yönetimi, stok takip, satış, müşteri ve personel yönetimi, raporlama ve analiz modüllerini içermektedir.",
         tools: ["C#", "MS SQL Server","Qt Designer","PyQt5","SQLite"]
     },
     {
         p_no: 6,
         p_name: "Ergonomik Risk Değerlendirme",
         p_img: project6_img,
-        description: "Ergonomi dersi kapsamında, çelik fabrikasında ergonomik risk değerlendirmesi gerçekleştirildi. REBA metodunun uygulanması için CATIA’daki dijital insan modelleme aracıyla çalışma duruşları simüle edildi. Mevcut durum analiz edilerek, çalışan konforunu artırmak amacıyla yenilikçi paketleme makinesi tasarlandı.",
+        description: "Ergonomi dersi kapsamında, çelik fabrikasında ergonomik risk değerlendirmesi gerçekleştirdik. REBA metodunun uygulanması için CATIA’daki dijital insan modelleme aracıyla çalışma duruşları simüle edildi. Mevcut durum analiz edilerek, çalışan konforunu artırmak amacıyla yenilikçi paketleme makinesi tasarlandı.",
         tools: ["CATIA", "SOLIDWORKS"]
     },
 ]
